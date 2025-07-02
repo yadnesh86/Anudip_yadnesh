0100edc7b382769d90033c27ab2a2afb4a3d25f4
unknown <yadneshpatil887@gmail.com>
2025-07-02 16:45:07 +0530

    first commit

---
+++ Person.java
@@ -0,0 +1,13 @@
+public class Person{
+    // variables 
+static String name = "kalyani";
+static int age = 31;
+
+public static void main(String[] args){
+    System.out.println("Welcome");
+    System.out.println(name);
+    System.out.println(age);
+
+    System.out.println("Welcome " + name + age);
+}
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+echo "# Anudip_yadnesh" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yadnesh86/Anudip_yadnesh.git
+git push -u origin main
